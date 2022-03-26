a46a24921a44d36287f2828b7d1745de748f0403
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2022-03-26 12:11:47 +0530

    setup

---
+++ src/navigations/Startup.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Startup() {
+  return (
+    <View>
+      <Text>Startup</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
