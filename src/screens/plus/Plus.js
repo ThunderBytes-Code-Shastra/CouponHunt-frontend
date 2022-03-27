7cb98e334178473558b431cbebd84fdc96349cf5
dhaval-76 <solankidhavals982@gmail.com>
2022-03-27 07:14:06 +0530

    add credit card page

--- src/screens/plus/Plus.js
+++ src/screens/plus/Plus.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,9 +7,11 @@ import {
   Image,
   Dimensions,
   FlatList,
+  Switch,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useDispatch, useSelector } from "react-redux";
+import { CreditCardInput } from "react-native-credit-card-input";
 
 import Colors from "../../constant/Colors";
 
@@ -17,16 +19,54 @@ import {
   authIsAuthenticatedSelector,
   authNameSelector,
 } from "../../store/auth/selector";
+import CustomInput from "../../components/auth/CustomInput";
+
+import { Dropdown } from "react-native-element-dropdown";
+import { AntDesign } from "@expo/vector-icons";
+
+const data = [
+  { label: "Item 1", value: "1" },
+  { label: "Item 2", value: "2" },
+  { label: "Item 3", value: "3" },
+  { label: "Item 4", value: "4" },
+  { label: "Item 5", value: "5" },
+  { label: "Item 6", value: "6" },
+  { label: "Item 7", value: "7" },
+  { label: "Item 8", value: "8" },
+];
 
 const { width, height } = Dimensions.get("screen");
 
 export default Plus = ({ navigation }) => {
-  const bannerCarousel = useRef();
+  const [bankName, setBankName] = useState("");
+  const [isFocus, setIsFocus] = useState(false);
 
   const isAuthenticated = useSelector(authIsAuthenticatedSelector);
   const name = useSelector(authNameSelector);
+  const banks = useSelector();
 
   const dispatch = useDispatch();
+
+  const onChange = (formData) => {
+    console.log(JSON.stringify(formData, null, ""));
+  };
+
+  const onFocus = (field) => {
+    console.log("focusing: ", field);
+  };
+
+  const onSubmitHandler = () => {};
+
+  const renderLabel = () => {
+    if (bankName || isFocus) {
+      return (
+        <Text style={[styles.label, isFocus && { color: "blue" }]}>
+          Dropdown label
+        </Text>
+      );
+    }
+    return null;
+  };
 
   return (
     <View style={styles.container}>
@@ -37,7 +77,49 @@ export default Plus = ({ navigation }) => {
           backgroundColor: "#FFF",
         }}
       >
-        <Text>Plus</Text>
+        <CreditCardInput
+          autoFocus
+          requiresName={false}
+          requiresCVC={false}
+          requiresPostalCode={false}
+          labelStyle={styles.label}
+          inputStyle={styles.input}
+          validColor={"black"}
+          invalidColor={"red"}
+          placeholderColor={"darkgray"}
+          onFocus={onFocus}
+          onChange={onChange}
+        />
+        {renderLabel()}
+        <Dropdown
+          style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={data}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocus ? "Select item" : "..."}
+          searchPlaceholder="Search..."
+          value={bankName}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={(item) => {
+            setValue(item.value);
+            setIsFocus(false);
+          }}
+          renderLeftIcon={() => (
+            <AntDesign
+              style={styles.icon}
+              color={isFocus ? "blue" : "black"}
+              name="Safety"
+              size={20}
+            />
+          )}
+        />
       </ScrollView>
     </View>
   );
@@ -47,6 +129,51 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FFF",
+  },
+  switch: {
+    alignSelf: "center",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  label: {
+    color: "black",
+    fontSize: 12,
+  },
+  input: {
+    fontSize: 16,
+    color: "black",
+  },dropdown: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
   },
 });
 
